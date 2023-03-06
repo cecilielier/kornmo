d353b2e7f92daf802c1ed468b0acad3bd7923739
cecilie.lier@infuture.no <cecilie.lier@infuture.no>
2023-03-06 09:45:56 +0000

    Changed import

--- src/frost/frost_weather_sources.py
+++ src/frost/frost_weather_sources.py
@@ -1,17 +1,7 @@
 import requests
 import pandas as pd
-#from databricks.sdk.runtime import adlfs
-
-#def get_dbutils(spark):
-#        try:
-#            from pyspark.dbutils import DBUtils
-#            dbutils = DBUtils(spark)
-#        except ImportError:
-#            import IPython
-#            dbutils = IPython.get_ipython().user_ns["dbutils"]
-#        return dbutils
-#
-#dbutils = get_dbutils(spark)
+from pyspark.dbutils import DBUtils
+from pyspark.sql import SparkSession
 
 
 # ---------------- 1. Collects information about all weather station and sensors in Norway -----------------------------
